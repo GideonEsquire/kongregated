226b0a619261d3f89304772bf402fc3c7e1bed24
Gideon <GideonEsquire@gmail.com>
2022-04-24 12:26:39 -0700

    added bruins page and made a general mess

--- text.js
+++ text.js
@@ -16,39 +16,41 @@ function reddit_list(news) {
     var link = correct_url(obj.url)
 
     l.append(
-      $('<li></li>')
-        .append(
-          $("<p class = 're'></p>")
-            .append(
-              "<a class = 'title' target = '_blank' href = " +
-                'http://www.' +
-                obj.domain +
-                '></a>'
-            )
-            .append(
-              "<a target = '_blank' href = " +
-                link +
-                "><text class = 're-title'>" +
-                obj.title +
-                "</text><a/>  <a target = '_blank' style = 'color:grey;outline:0' href=http://www." +
-                obj.domain +
-                '>(' +
-                obj.domain +
-                ')</a>'
-            )
-        )
-        .append(
-          $(
-            "<p class='re-under'><a target = '_blank' href = " +
-              'http://reddit.com' +
-              obj.permalink +
-              " style = 'outline:0'><font color='grey'><b>(score: " +
-              obj.score +
-              ' | comments: ' +
-              obj.num_comments +
-              ') on reddit.com</b></font></a><p>'
+      $('<div class="wrapper"></div>').append(
+        $('<li></li>')
+          .append(
+            $("<p class = 're'></p>")
+              .append(
+                "<a class = 'title' target = '_blank' href = " +
+                  'http://www.' +
+                  obj.domain +
+                  '></a>'
+              )
+              .append(
+                "<a target = '_blank' href = " +
+                  link +
+                  "><text class = 're-title'>" +
+                  obj.title +
+                  "</text><a/>  <a target = '_blank' style = 'color:grey;outline:0' href=http://www." +
+                  obj.domain +
+                  '>(' +
+                  obj.domain +
+                  ')</a>'
+              )
           )
-        )
+          .append(
+            $(
+              "<p class='re-under'><a target = '_blank' href = " +
+                'http://reddit.com' +
+                obj.permalink +
+                " style = 'outline:0'><font color='grey'><b>(score: " +
+                obj.score +
+                ' | comments: ' +
+                obj.num_comments +
+                ') on reddit.com</b></font></a><p>'
+            )
+          )
+      )
     )
   })
 }
