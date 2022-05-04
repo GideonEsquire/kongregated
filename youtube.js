7f4b846831e7dee54d46a9d24450af05ac7901b9
Gideon <GideonEsquire@gmail.com>
2022-05-04 14:50:27 -0700

    added more yt sources. randomized yt script a little

--- youtube.js
+++ youtube.js
@@ -1,35 +1,36 @@
 if (!YT_USERS) {
   var YT_USERS = [
     'CGPGrey',
-    'onemeeeliondollars',
     'MrBeast6000',
     'Kurzgesagt',
     '1veritasium',
     'Vsauce',
-    'destinws2',
-    'steventhebrave',
     'TheRedNile',
     'jacksepticeye',
     'wheezywaiter',
-    // 'stuffmadehere',  <-- I don't know the user name
-    // 'mkbhd',  <-- find the user name
-    // 'pewidepie',  <-- find the user name
-    // 'lastweektonight',  <-- find the user name
-    // 'two minute papers',  <-- find the user name
-    // 'joshua weissman',  <-- find the user name
-    // 'abroad in japan',  <-- find the user name
-    // 'wall street journal',  <-- find the user name
-    // 'lockpicking lawyer',  <-- find the user name
-    // 'electroboom',  <-- find the user name
-    // 'diy perks',  <-- find the user name
-    // 'handcraft',  <-- find the user name
-    // 'jerryrigeverything',  <-- find the user name
-    // 'the slomo guys',  <-- find the user name
-    // 'practical engineering',  <-- find the user name
-    // 'the b1m',  <-- find the user name
-    // 'adam savage tested',  <-- find the user name
-    // 'binging with babish',  <-- find the user name
-    // 'scishow',  <-- find the user name
+    'LastWeekTonight',
+    'PewDiePie',
+    'onemeeeliondollars', // 'mark rober'
+    'destinws2', // 'smarter every day'
+    'steventhebrave', // 'steve mould'
+    'cmbroad44', // 'abroad in japan'
+    'keeroyz', // 'two minute papers'
+    'marquesbrownlee', // 'mkbhd'
+    'scishow', // 'scishow'
+    'testedcom', // 'adam savage tested'
+    'WSJDigitalNetwork',// 'wall street journal'
+    'msadaghd', // 'electroboom'
+    'DIYPerks', // 'diy perks'
+    'TheB1MLtd', // 'the b1m'
+    'JerryRigEverything', // 'jerryrigeverything'
+    'theslowmoguys', // 'the slomo guys'
+    'gradyhillhouse', // 'practical engineering'
+
+    // 'binging with babish'<-- I don't know the user name
+    // 'stuffmadehere',<-- I don't know the user name
+    // 'joshua weissman'<-- I don't know the user name
+    // 'lockpicking lawyer'<-- I don't know the user name
+    // 'handcraft'<-- I don't know the user name
   ]
 }
 
@@ -39,15 +40,18 @@ var x = $('#yt .list-unstyled')
 $(' #yt .list-unstyled .wrapper').remove()
 
 $.each(YT_USERS, function (index, obj) {
-  x.append(
-    $('<div class="wrapper"></div>').append(
-      $('<li></li>').append(
-        '<iframe width="600" height="340" ' +
-          'src="https://www.youtube.com/embed?max-results=1&controls=1&' +
-          'showinfo=0&rel=0&listType=user_uploads&list=' +
-          obj +
-          '" frameborder="0" allowfullscreen></iframe>'
+  let r = Math.random()
+  if (r > 0.5) {
+    x.append(
+      $('<div class="wrapper"></div>').append(
+        $('<li></li>').append(
+          '<iframe width="600" height="340" ' +
+            'src="https://www.youtube.com/embed?max-results=1&controls=1&' +
+            'showinfo=0&rel=0&listType=user_uploads&list=' +
+            obj +
+            '" frameborder="0" allowfullscreen></iframe>'
+        )
       )
     )
-  )
+  }
 })
