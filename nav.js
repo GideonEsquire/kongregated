5c108827b40da46205c0e087120ff30653da2b9d
Gideon <GideonEsquire@gmail.com>
2022-05-19 13:21:14 -0700

    fixed some scoll and nav misbehavior

--- nav.js
+++ nav.js
@@ -1,15 +1,17 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementsByClassName("btn")[0].style.top = "0";
-  } else {
-    document.getElementsByClassName("btn")[0].style.top = "-50px";
+var prevScrollpos = window.pageYOffset
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset
+  setTimeout(() => {
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementsByClassName('btn')[0].style.top = '0'
+    } else {
+      document.getElementsByClassName('btn')[0].style.top = '-50px'
 
-    // Can't quite figure this out
-    // document.getElementsByClassName("sidebar")[0].style.left = "-250px";
-    // document.getElementsByClassName("sidebar")[0].style.transform = "translateX(-250px)";
-    // console.log(document.getElementsByClassName("sidebar")[0])
-  }
-  prevScrollpos = currentScrollPos;
+      // Can't quite figure this out
+      // document.getElementsByClassName("sidebar")[0].style.left = "-250px";
+      // document.getElementsByClassName("sidebar")[0].style.transform = "translateX(-250px)";
+      // console.log(document.getElementsByClassName("sidebar")[0])
+    }
+    prevScrollpos = currentScrollPos
+  }, 100)
 }
