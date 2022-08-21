25c47774d59cde11a1692a5ef1ba4178f8502807
Gideon <GideonEsquire@gmail.com>
2022-08-21 14:47:00 -0400

    refactor sidebar. this breaks it a little. non-dynamic right now

---
+++ footer.js
@@ -0,0 +1,7 @@
+$('footer').append(
+  '<ul> <li><a href="/about">About</a></li>' +
+    '<li><a href="/contact">Contact</a></li>' +
+    '<li><a href="/license">License</a></li>' +
+    '<li><a href="/disclaimer">Disclaimer</a></li>' +
+  '</ul>'
+)
