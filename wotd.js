79275480f309b1738ed8f5db7b5a1fb891b6d4e5
Gideon <GideonEsquire@gmail.com>
2022-09-14 13:25:57 -0400

    reshuffle. nothing really implemented

--- wotd.js
+++ wotd.js
@@ -1,23 +1,7 @@
-// This is still buggy
-
 // Create a new object to interact with the server
 var xhr = new XMLHttpRequest()
 
-// I'd like to get a new topic each day
-// Perhaps I need to get a random page and change the seed daily...
-var topic = 'triangle'
-
-var url =
-  'https://simple.wikipedia.org/w/api.php' +
-  '?action=query' +
-  '&prop=extracts' +
-  '&origin=*' +
-  '&exsentences=4' +
-  '&exlimit=1' +
-  '&titles=' +
-  topic +
-  '&explaintext=1' +
-  '&format=json'
+var url = 'https://www.merriam-webster.com/word-of-the-day'
 
 // Provide 3 arguments (GET/POST, The URL, Async True/False)
 xhr.open('GET', url, true)
@@ -25,13 +9,12 @@ xhr.open('GET', url, true)
 // Once request has loaded...
 xhr.onload = function() {
   let data = JSON.parse(this.response)
-  let pages = data.query.pages
-  let pid = Object.keys(pages)[0]
-  let content = pages[pid].extract
-  wiki_topic(content)
+  console.log(data)
+
+  make_dom(data)
 }
 
-function wiki_topic(content) {
+function make_dom(content) {
   let l = $('#posts .list-unstyled')
   let link = 'https://simple.wikipedia.org/wiki/' + topic
   let domain = 'wikipedia.org'
