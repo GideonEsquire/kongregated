dd5b1f0885a654c2efa432c16f6b067e4f121cd7
Gideon <GideonEsquire@gmail.com>
2022-09-19 13:59:07 -0400

    slight tweak to shuffle formatting

--- shuffle.js
+++ shuffle.js
@@ -1,10 +1,10 @@
 function shuffle() {
-  var cards = $('.wrapper')
-  for (var i = 0; i < cards.length; i++) {
-    var target = Math.floor(Math.random() * cards.length - 1) + 1
-    var target2 = Math.floor(Math.random() * cards.length - 1) + 1
+  let cards = $('.wrapper')
+  Object.keys(cards).forEach(() => {
+    let target = Math.floor(Math.random() * cards.length - 1) + 1
+    let target2 = Math.floor(Math.random() * cards.length - 1) + 1
     cards.eq(target).before(cards.eq(target2))
-  }
+  })
 }
 
 setTimeout(() => shuffle(), 2000)
