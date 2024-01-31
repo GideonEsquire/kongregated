2fbe4b7838a1c38a4e4e10a0cfdd8e5c7e5c6f0e
Gideon <GideonEsquire@gmail.com>
2024-01-31 13:12:56 -0800

    added a path for Nicki
    This hasn't been personalized yet
    I'd like to hit the pokeapi
    Link some common YouTube videos
    And think of some other things for her

---
+++ nicki/nicki.js
@@ -0,0 +1,108 @@
+var SUBREDDITS = {
+  aframes: 1,
+  anime_titties: 3,
+  archlinux: 1,
+  Art: 1,
+  ArtisanVideos: 1,
+  AskEngineers: 1,
+  AskReddit: 1,
+  bash: 2,
+  blender: 1,
+  boardgames: 1,
+  books: 1,
+  BostonBruins: 1,
+  compsci: 1,
+  DepthHub: 1,
+  Design: 1,
+  DesignPorn: 1,
+  DigitalArt: 1,
+  discworld: 1,
+  DIY: 1,
+  docker: 1,
+  educationalgifs: 1,
+  ErgoMechKeyboards: 1,
+  explainlikeimfive: 1,
+  Foodforthought: 1,
+  Futurology: 1,
+  gardening: 1,
+  ImaginaryInteriors: 1,
+  ImaginarySkyscapes: 1,
+  ImaginarySliceOfLife: 1,
+  ImaginaryWeather: 1,
+  ImaginaryWorlds: 1,
+  linux: 1,
+  low_poly: 1,
+  mildlyinteresting: 1,
+  minimalism: 1,
+  movies: 1,
+  neovim: 1,
+  netsec: 1,
+  networking: 1,
+  news: 1,
+  OutOfTheLoop: 1,
+  productivity: 1,
+  ProgrammerHumor: 1,
+  programming: 1,
+  rust: 1,
+  science: 1,
+  Seattle: 1,
+  sysadmin: 1,
+  technology: 1,
+  todayilearned: 1,
+  typescript: 1,
+  UpliftingNews: 1,
+  vim: 1,
+  vuejs: 1,
+  walkablecities: 1,
+  woodworking: 1,
+  worldnews: 1,
+  YouShouldKnow: 1,
+}
+
+var YT_USERS = [
+    '9RM-iSvTu1uPJb8X5yp3EQ', //'Wendover productions'
+    '2C_jShtL725hvbm1arSV9w', //'CGPGrey'
+    'sXVk37bltHxD1rDPwtNM8Q', //'Kurzgesagt'
+    'HnyfMqiRRG1u-2MsSQLbXA', //'1veritasium'
+    'FhXFikryT4aFcLkLw2LBLA', //'TheRedNile'
+    'QL5ABUvwY7YoW5lgMyAS_w', //'wheezywaiter'
+    '3XTzVzaHQEd30rQbuvCtTQ', //'LastWeekTonight'
+    'Y1kMZp36IQSyNx_9h4mpCg', //'mark rober'
+    '6107grRI4m0o2-emgoDnAA', //'smarter every day'
+    'EIwxahdLz7bap-VDs9h35A', //'steve mould'
+    'HL9bfHTxCMi-7vfxQ-AYtg', //'abroad in japan'
+    'bfYPyITQ-7l4upoX8nvctg', //'two minute papers'
+    'BJycsmduvYEL83R_U4JriQ', //'mkbhd'
+    'UdettijNYvLAm4AixZv4RA', //'scishow'
+    'iDJtJKMICpb9B1qf7qjEOA', //'adam savage tested'
+    'K7tptUDHh-RYDsdxO1-5QQ', //'wall street journal'
+    'J0-OtVpF0wOKEqT2Z1HEtA', //'electroboom'
+    'UQo7nzH1sXVpzL92VesANw', //'diy perks'
+    '6n8I1UDTKP1IWjQMg6_TwA', //'the b1m'
+    'WFKCr40YwOZQx8FHU_ZqqQ', //'jerryrigeverything'
+    'UK0HBIBWgM2c4vsPhkYY4w', //'the slomo guys'
+    'MOqf8ab-42UUQIdVoKwjlQ', //'practical engineering'
+    'JHA_jMfCvEnv-3kRjTCQXw', //'binging with babish'
+    'j1VqrHhDte54oLgPG4xpuQ', //'stuffmadehere'
+    'm9K6rby98W8JigLoZOh6FQ', //'lockpicking lawyer'
+    'Eyr6FYPqXj3agOBSBPyLWw', //'handcraft'
+    'uVLG9pThvBABcYCm7pkNkA', //'climate town'
+    '0intLFzLaudFG-xAvUEO-A', //'not just bikes'
+    'fgtNfWCtsLKutY-BHzIb9Q', //'city nerd'
+    'qqJQ_cXSat0KIAVfIfKkVA', //'j kenji lopez-alt'
+    'N3aYbtQ7yCqk9DM56B0kEw', //'tomorrow's build'
+    'Dq5v10l4wkV5-ZBIJJFbzQ', //'Ethan chlebowski'
+    'y0tKL1T7wFoYcxCe0xjN6Q', //'Technology connections'
+    'Bw0bL8MtOIl1UX7db2MT3A', //'Always sunny podcast'
+    'uZ4plJK1KkSqRBYps3rNww', //'Mr. Puzzle'
+    'Xy10-NEFGxQ3b4NVrzHw1Q', //'Eric rosen'
+    'oal_hpPIPAnWlG-kWHLheA', //'drawfee'
+    'C-UOdK8-mIjxBQm_ot1T-Q', //'CTC'
+    'YO_jab_esuFRV4b17AJtAw', //'3blue1brown'
+    'vjgXvBlbQiydffZU7m1_aw', //'Coding Train'
+    'UHW94eEFW7hkUMVaZz4eDg', //'minute physics'
+    'Kp44bWWZIiOPShPN_ytShw', //'Wood whisperer'
+    'X6b17PVsYBQ0ip5gyeme-Q', //'crash course'
+]
+
+var ODDS = 0.7
